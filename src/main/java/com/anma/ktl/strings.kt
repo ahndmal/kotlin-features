5de9e211266567b9af1094dfe0e3a10ae736aa5b
Andrii Maliuta <quadr988@gmail.com>
2020-12-30 12:39:52 +0200

    init

--- src/main/java/com/anma/ktl/strings.kt
+++ src/main/java/com/anma/ktl/strings.kt
@@ -1,5 +1,7 @@
 package com.anma.ktl
 
+
+
 fun main() {
 
     val str1: String = "Hello";
@@ -11,8 +13,14 @@ fun main() {
     val array = toSplit.split(" ")
     val array2 = name.split("")
     val toStr1 = array2.joinToString(separator = "")
-    println(array2.toString())
-    println(toStr1)
+//    println(array2.toString())
+//    println(toStr1)
+
+    fun retStr(yearsOld: String): Int {
+        return Integer.parseInt(yearsOld.substring(0, 1))
+    }
+
+    print(retStr("4 years old"))
 
 //    println ("$str1 $name")
 //    println(name[4])
--- src/main/java/com/anma/ktl/test2.kt
+++ src/main/java/com/anma/ktl/test2.kt
@@ -1,5 +1,11 @@
 package com.anma.ktl
 
+fun getAge(inputString: String): Int{
+
+    return Integer.parseInt(inputString.substring(0, 1))
+
+}
+
 fun main() {
 
     print(3.14 * Math.pow(12.0, 2.0).toInt())}
